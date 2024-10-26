01c7cd5fb0354581f2391a5fa643addd63f360a2
zhangyao <3518924564@qq.com>
2024-10-26 18:23:32 +0800

    feat: 实现剩下的两个内容

--- lab0/lab.py
+++ lab0/lab.py
@@ -9,11 +9,30 @@ def sum_digits(y: int) -> int:
     """
     Sum all the digits of y.
     """
-    pass
+    
+    sum = 0
+    while y>0:
+        digit=y%10
+        y//=10
+        sum+=digit
+    return sum
+
 
 
 def double_eights(n: int) -> bool:
     """
     Return True if n has two eights in a row.
     """
-    pass
+    
+    prev_eight=False
+    while n>0:
+        digit =n%10
+        if digit==8:
+            if prev_eight:
+                return True
+            else:
+                prev_eight=True
+        else:
+            prev_eight=False
+        n//=10
+    return False
